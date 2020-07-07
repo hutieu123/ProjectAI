62cd82e81154f9af1d45673a99489649004e0c57
hearterzouest <hearterzouest99.999@gmail.com>
2020-07-07 21:17:10 +0700

    Init Try Change Config

--- Project_Caro/src/project/caro/config/ConfigGame.java
+++ Project_Caro/src/project/caro/config/ConfigGame.java
@@ -1,11 +1,29 @@
 package project.caro.config;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class ConfigGame {
 	public static final int DRAW = 50;
-	public static final int TARGET_X = 1;
-	public static final int TARGET_O = 2;
+	
+	
 	public static final int NUMBER_WIN = 5;
 	public static final int NUMBER_COLS = 15;
 	public static final int NUMBER_ROWS = 15;
 	public static final int TIME_OF_TURN = 10;
+	
+	
+	public static final int DEPTH = 1;
+	public enum Status{X_WIN_GAME,X_LOSE_GAME,O_WIN_GAME,O_LOSE_GAME, NOT_OVER, STALEMATE};
+	public enum StatusMinimax{WIN_GAME, LOSE_GAME, NOT_OVER, STALEMATE};
+	public static class X {
+		public static final int VALUE = 1;
+	}
+	public static class O{
+		public static final int VALUE = 2;
+	}
+	public static class NOT_THING{
+		public static final int VALUE = -1;
+	}
 }
